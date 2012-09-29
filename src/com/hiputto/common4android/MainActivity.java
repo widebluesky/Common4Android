6c240ad76934f3eedee993d93365ee78b2776264
widebluesky <widebluesky@qq.com>
2012-09-29 14:35:57 +0800

    version 20120929

--- src/com/hiputto/common4android/MainActivity.java
+++ src/com/hiputto/common4android/MainActivity.java
@@ -1,7 +1,12 @@
 package com.hiputto.common4android;
 
 import com.hiputto.common4android.superclass.HP_BaseActivity;
+import com.hiputto.common4android.util.HP_NetWorkAsyncTask;
+import com.hiputto.common4android.util.HP_NetWorkUtils;
+import com.hiputto.common4android.util.HP_NetWorkUtils.OnRequestFinished;
+
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends HP_BaseActivity {
 
@@ -9,6 +14,23 @@ public class MainActivity extends HP_BaseActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		HP_NetWorkUtils hp_NetWorkUtils = new HP_NetWorkUtils();
+		String url = "http://www.baidu.com";
+		
+		HP_NetWorkAsyncTask hp_NetWorkAsyncTask = (HP_NetWorkAsyncTask) hp_NetWorkUtils.sendAsyncRequest(url, new OnRequestFinished() {
+			
+			@Override
+			public void onRequestFinished(String resultStr, boolean isSuccess) {
+				
+				Log.d("ads", "asdf"+resultStr);
+			
+			}
+		});
+		
+		
+		
 	}
 
 }
