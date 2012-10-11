27f96e86a22ba7ab43ce044b29d5dbffff42d1a4
widebluesky <widebluesky@qq.com>
2012-10-11 11:58:12 +0800

    Update HP_AnimationUtils.java Refresh.

--- gen/com/hiputto/common4android/R.java
+++ gen/com/hiputto/common4android/R.java
@@ -13,16 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int image=0x7f020002;
     }
     public static final class id {
         public static final int button=0x7f070000;
-        public static final int button_reset=0x7f070001;
-        public static final int edit_text_linearlayout1=0x7f070003;
-        public static final int edit_text_linearlayout2=0x7f070004;
-        public static final int edit_text_linearlayout3=0x7f070005;
-        public static final int edit_text_linearlayout4=0x7f070006;
-        public static final int imageView=0x7f070002;
-        public static final int menu_settings=0x7f070007;
+        public static final int imageView=0x7f070001;
+        public static final int menu_settings=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/hiputto/common4android/MainActivity.java
+++ src/com/hiputto/common4android/MainActivity.java
@@ -1,33 +1,22 @@
 package com.hiputto.common4android;
 
 import com.hiputto.common4android.superclass.HP_BaseActivity;
-import com.hiputto.common4android.util.HP_BitmapUtils;
-import com.hiputto.common4android.util.HP_NetWorkUtils;
-import com.hiputto.common4android.util.HP_NetWorkUtils.OnRequestBitmapFinished;
+import com.hiputto.common4android.util.HP_AnimationUtils;
 
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.ColorMatrix;
-import android.graphics.ColorMatrixColorFilter;
-import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class MainActivity extends HP_BaseActivity {
 
 	private ImageView imageView;
-	private LinearLayout linearLayout1,linearLayout2,linearLayout3,linearLayout4;
-	private Button button,resetButton;
-	
-	private Bitmap bm ;
-	
-	private float []carray=new float[20]; 
+	private Button button;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -35,151 +24,54 @@ public class MainActivity extends HP_BaseActivity {
 		setContentView(R.layout.activity_main);
 		
 		imageView = (ImageView) findViewById(R.id.imageView);
-		linearLayout1 = (LinearLayout) findViewById(R.id.edit_text_linearlayout1);
-		linearLayout2 = (LinearLayout) findViewById(R.id.edit_text_linearlayout2);
-		linearLayout3 = (LinearLayout) findViewById(R.id.edit_text_linearlayout3);
-		linearLayout4 = (LinearLayout) findViewById(R.id.edit_text_linearlayout4);
+		imageView.setVisibility(View.INVISIBLE);
 		button = (Button) findViewById(R.id.button);
-		resetButton = (Button) findViewById(R.id.button_reset);
 		
-		
-		String imageUrl = "http://m2.img.libdd.com/farm4/2012/1008/09/CCCA769D1889542204D0ECBB0DFA0F19148484189977_500_316.jpg";
-		
-		HP_NetWorkUtils hp_NetWorkUtils = new HP_NetWorkUtils();
-		
-		hp_NetWorkUtils.getAsyncRequestBitmap(imageUrl, new OnRequestBitmapFinished() {
-			
-			@Override
-			public void onRequestBitmapFinished(String resultStr, Bitmap bitmap,
-					boolean isSuccess) {
-				bm = HP_BitmapUtils.bitmap2Round(bitmap, (float) 20.0);
-				bm = HP_BitmapUtils.bitmap2WatermarkText(bm, "asdfasdfaasdfadsfdsafdasfadsasdfdasfad", Color.RED, 50, 0, 0,true);
-				
-				imageView.setImageBitmap(bm);
-			}
-		});
-		
-		resetButton.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				imageView.setImageBitmap(bm);
-				
-			}
-		});
 		button.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View arg0) {
-				EditText et = (EditText) linearLayout1.getChildAt(0);//1
-				carray[0] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout1.getChildAt(1);//0
-				carray[1] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout1.getChildAt(2);//0
-				carray[2] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout1.getChildAt(3);//0
-				carray[3] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout1.getChildAt(4);//0
-				carray[4] = Float.valueOf(et.getText().toString());
-				
-				et = (EditText) linearLayout2.getChildAt(0);//0
-				carray[5] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout2.getChildAt(1);//1
-				carray[6] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout2.getChildAt(2);//0
-				carray[7] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout2.getChildAt(3);//0
-				carray[8] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout2.getChildAt(4);//0
-				carray[9] = Float.valueOf(et.getText().toString());
-				
-				et = (EditText) linearLayout3.getChildAt(0);//0
-				carray[10] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout3.getChildAt(1);//0
-				carray[11] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout3.getChildAt(2);//1
-				carray[12] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout3.getChildAt(3);//0
-				carray[13] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout3.getChildAt(4);//0
-				carray[14] = Float.valueOf(et.getText().toString());
-				
-				et = (EditText) linearLayout4.getChildAt(0);//0
-				carray[15] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout4.getChildAt(1);//0
-				carray[16] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout4.getChildAt(2);//0
-				carray[17] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout4.getChildAt(3);//1
-				carray[18] = Float.valueOf(et.getText().toString());
-				et = (EditText) linearLayout4.getChildAt(4);//0
-				carray[19] = Float.valueOf(et.getText().toString());
-				
-				Bitmap bitmap = Bitmap.createBitmap(bm);
-				Paint paint = new Paint(); 
-				paint.setColorFilter(null); 
-				Canvas canvas = new Canvas(bitmap);
-				ColorMatrix cm = new ColorMatrix(); 
-				//设置颜色矩阵 
-				cm.set(carray); 
-				//颜色滤镜，将颜色矩阵应用于图片 
-				paint.setColorFilter(new ColorMatrixColorFilter(cm)); 
-				//绘图 
-				canvas.drawBitmap(bitmap, 0, 0, paint); 
-				
-				imageView.setImageBitmap(bitmap);
+				//播放动画
+				imageView.startAnimation(new HP_AnimationUtils().animAlpha(0.1f, 1.0f, 500,new AnimationListener() {
+					
+					@Override
+					public void onAnimationStart(Animation animation) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onAnimationRepeat(Animation animation) {
+						// TODO Auto-generated method stub
+						
+					}
+					
+					@Override
+					public void onAnimationEnd(Animation animation) {
+//						imageView.getLayoutParams().;
+//						params.
+						
+						LayoutParams params = (LayoutParams) imageView.getLayoutParams();
+						params.height = 0;
+						params.width = 0;
+						imageView.setLayoutParams(params);
+						imageView.clearAnimation();
+						
+					}
+				}));
+			}
+		});
+		
+		imageView.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.e("click", "click");
 			}
 		});
 		
 	}
 	
-	/**
-	 * 怀旧效果(相对之前做了优化快一倍)
-	 * @param bmp
-	 * @return
-	 */
-	private Bitmap oldRemeber(Bitmap bmp)
-	{
-		// 速度测试
-		int width = bmp.getWidth();
-		int height = bmp.getHeight();
-		Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-		int pixColor = 0;
-		int pixR = 0;
-		int pixG = 0;
-		int pixB = 0;
-		int newR = 0;
-		int newG = 0;
-		int newB = 0;
-		int[] pixels = new int[width * height];
-		bmp.getPixels(pixels, 0, width, 0, 0, width, height);
-		
-		for (int i = 0; i < height; i++)
-		{
-			for (int k = 0; k < width; k++)
-			{
-				pixColor = pixels[width * i + k];
-				pixR = Color.red(pixColor);
-				pixG = Color.green(pixColor);
-				pixB = Color.blue(pixColor);
-				
-//				R' = a*R + b*G + c*B + d*A + e;
-//				G' = f*R + g*G + h*B + i*A + j;
-//				B' = k*R + l*G + m*B + n*A + o;
-//				A' = p*R + q*G + r*B + s*A + t;
-				
-				newR = (int) (0.393 * pixR + 0.769 * pixG + 0.189 * pixB);
-				newG = (int) (0.349 * pixR + 0.686 * pixG + 0.168 * pixB);
-				newB = (int) (0.272 * pixR + 0.534 * pixG + 0.131 * pixB);
-				
-				int newColor = Color.argb(255, newR > 255 ? 255 : newR, newG > 255 ? 255 : newG, newB > 255 ? 255 : newB);
-				pixels[width * i + k] = newColor;
-
-			}
-		}
-		
-		bitmap.setPixels(pixels, 0, width, 0, 0, width, height);
-		return bitmap;
-	}
 
 }
