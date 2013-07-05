5a670ce60476a201d7f150814c9ffa69c8e35fa8
Yi Xu <widebluesky@qq.com>
2013-07-05 18:15:27 +0800

    [提交] 1.重新编译。

--- gen/com/hiputto/common4android/R.java
+++ gen/com/hiputto/common4android/R.java
@@ -13,7 +13,6 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
-        public static final int image=0x7f020002;
     }
     public static final class id {
         public static final int main_layout=0x7f070001;
